b6ca9959d1b4d38305f336d0017e5dcb024697d5
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2023-04-28 02:17:12 +0200

    addedAuthentificationSeederLogicWithLaravelBreeze

---
+++ planning-tool/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'docent',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('backendisawesome'),
+            'is_admin' => true,
+        ]);
+    }
+}
---
+++ planning-tool/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<h2>welcome page</h2>
+<button>
+<a href="{{ route('login') }}" >
+    Login
+</a>
+</button>
+
+<button>
+
+<a href="{{ route('register') }}" >
+    Register
+</a>
+</button>
